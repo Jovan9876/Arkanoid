6f9df5d24bacac04b4027cbaa0d0d1a2d57aff12
Jovan <jovan@Jovans-Mac.lan>
2025-02-16 20:07:20 -0800

    first commit

---
+++ Arkanoid/Arkanoid/Arkanoid Shared/Box2DBridge.h
@@ -0,0 +1,21 @@
+//
+//  Box2DBridge.h
+//  Arkanoid
+//
+//  Created by Jovan on 2025-02-16.
+//
+#ifndef Box2DBridge_h
+#define Box2DBridge_h
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "box2d/box2d.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
---
+++ Arkanoid/Arkanoid/Arkanoid Shared/Box2DBridge.m
@@ -0,0 +1,9 @@
+//
+//  Box2DBridge.m
+//  Arkanoid iOS
+//
+//  Created by Jovan on 2025-02-16.
+//
+
+#import "Box2DBridge.h"
+
